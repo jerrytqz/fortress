c4f897d761345b203f3a498040c4e2affa3a5d5f
JerryZhengPro <jerryzhengpro@gmail.com>
2022-08-03 22:07:13 -0700

    Fix typos

--- spin_backend/asgi.py
+++ spin_backend/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spin_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spin_backend.production_settings')
 
 application = get_asgi_application()
--- spin_backend/wsgi.py
+++ spin_backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spin_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spin_backend.production_settings')
 
 application = get_wsgi_application()
