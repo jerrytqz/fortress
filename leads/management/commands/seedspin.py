a88d6b433c974b02c7f4cfe92bdb0d9802da421a
jerrytqz <jerryzhengpro@gmail.com>
2022-09-27 16:23:48 -0400

    Update seedspin.py

--- leads/management/commands/seedspin.py
+++ leads/management/commands/seedspin.py
@@ -1,34 +1,38 @@
 from django.core.management.base import BaseCommand
-
-from pathlib import Path
-import csv
 from leads.models import Item
 
-BASE_PATH = Path(__file__).parent 
-CSV_PATH = (BASE_PATH / '../../items.csv').resolve()
+import csv
+import urllib.request
+
+CSV_PATH = 'https://starship.jerrytq.com/spin/items/items.csv'
 
 class Command(BaseCommand):
     help = 'Seeds the Spin database with items'
 
     def handle(self, *args, **kwargs):
-        with open(CSV_PATH, 'r', newline='', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            
-            # Skip header
-            next(reader)
+        try:
+            response = urllib.request.urlopen(CSV_PATH)
+            file = [l.decode('utf-8') for l in response.readlines()]
+        except Exception as e:
+            print("Could not open seeder file")
+            return
+        
+        reader = csv.reader(file)
+        next(reader) # Skip header
 
-            for i, row in enumerate(reader):
-                try:
-                    item, created = Item.objects.update_or_create(
-                        name=row[0],
-                        defaults={'name': row[0], 'rarity': row[1], 'description': row[2]}
-                    )
+        for i, row in enumerate(reader):
+            try:
+                item, created = Item.objects.update_or_create(
+                    name=row[0],
+                    defaults={'name': row[0], 'rarity': row[1], 'description': row[2]}
+                )
+            
+                if created:
+                    item.rarity = row[1]
+                    item.description = row[2]
+                    item.in_circulation = 0
+                    item.save()
                 
-                    if created:
-                        item.rarity = row[1]
-                        item.description = row[2]
-                        item.in_circulation = 0
-                        item.save()
-                except Exception as e:
-                    print("Failed in row {} with {}".format(i + 2, e)) # Define header as row 1
-                    break
+            except Exception as e:
+                print("Failed in row {} with {}".format(i + 2, e)) # Define header as row 1
+                break
