339edff3a4526e7882301528819835ea8d768dde
jerrytqz <jerryzhengpro@gmail.com>
2022-12-19 22:32:03 -0800

    Set up project implementation for JerryTQ app

--- jerrytq/views.py
+++ jerrytq/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-from jerrytq import models
+from jerrytq.models import Project
 
 def fetch_project_names(request):
     if request.method != 'GET':
@@ -9,7 +9,18 @@ def fetch_project_names(request):
 
     response = []
 
-    for project in models.Project.objects.all():
+    for project in Project.objects.all():
         response.append({'name': project.name, 'urlName': project.url_name})
 
     return JsonResponse({'projectNames': response})
+
+def fetch_project(request):
+    if request.method != 'GET':
+        return JsonResponse({'error': "Request error"}, status=400)
+    
+    project_url_name = request.GET.get('urlName', '')
+
+    if not Project.objects.filter(url_name=project_url_name).exists():
+        return JsonResponse({'error': "There are currently no projects with that name."}, status=404);
+    
+    return JsonResponse({'hello': 'world'});
